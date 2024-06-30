8f5398e1327545bc4cc828bb2871aa256863424d
Carlos Paredes <146150706+YCocos@users.noreply.github.com>
2024-06-30 17:15:15 -0600

    Delete Proyecto/CRUDProducto/domain/use_cases/uc_post_product_SQS.mjs

--- Proyecto/CRUDProducto/domain/use_cases/uc_post_product_SQS.mjs
+++
@@ -1,9 +0,0 @@
-import { post_product_SQS } from "../../domain/repositories/post_product_SQS.mjs";
-
-export const uc_post_product_SQS = async (stage) => {
-    let responseEvent = "No encontrado";
-
-    responseEvent = await post_product_SQS(stage);
-
-    return responseEvent;
-}
